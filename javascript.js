acc4c49f5a0bc8f8f5bf384b6eb0a0a50c179b2a
LorenzoBennie <131253527+LorenzoBennie@users.noreply.github.com>
2023-05-30 09:46:55 +0200

    Create function that executes 1 round of the game

--- javascript.js
+++ javascript.js
@@ -5,3 +5,72 @@ function getComputerChoice () {
     return computerChoice;
 }
 
+//function that executes one round of the game//
+function playRound (playerSelection,computerSelection) {
+
+// Allows the player to choose their "weapon" via a prompt//
+  playerSelection = prompt("Choose your weapon!",);
+  let firstChar = playerSelection[0];
+  let otherChar = playerSelection.slice(1,);
+  firstChar = firstChar.toUpperCase();
+  otherChar = otherChar.toLowerCase();
+  playerSelection = firstChar + otherChar;
+  
+  //envokes the ComputerChoice function and compares it with the player choice, returning a string//
+  computerSelection  = getComputerChoice();
+
+  console.log(playerSelection);
+  console.log(computerSelection);
+
+  //executes if player chooses Rock//
+  if (playerSelection === "Rock") {
+    switch(computerSelection) {
+      case "Rock":
+        return "It's a tie!";
+        break;
+
+      case "Paper":
+        return "Paper covers rock. You lose!"
+        break;
+        
+      case "Scissors":
+        return "Rock breaks scissors. You win!"  
+
+    }    
+  }
+
+  //executes if player chooses Paper//
+  if (playerSelection === "Paper") {
+    switch(computerSelection) {
+      case "Paper":
+        return "It's a tie!";
+        break;
+
+      case "Rock":
+        return "Paper covers rock. You win!"
+        break;
+        
+      case "Scissors":
+        return "Scissors cuts paper. You lose!"  
+
+    }    
+  }
+
+  //executes if player chooses Scissors//
+  if (playerSelection === "Scissors") {
+    switch(computerSelection) {
+      case "Scissors":
+        return "It's a tie!";
+        break;
+
+      case "Rock":
+        return "Rock breaks scissors. You lose!"
+        break;
+        
+      case "Paper":
+        return "Scissors cuts paper. win!"  
+
+    }    
+  }  
+    
+}
