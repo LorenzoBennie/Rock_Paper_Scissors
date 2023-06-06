63afe3cbbc06b940cf6739b908114f86407fa854
LorenzoBennie <131253527+LorenzoBennie@users.noreply.github.com>
2023-06-06 19:44:49 +0200

    Add a UI to the game

--- javascript.js
+++ javascript.js
@@ -57,60 +57,63 @@ function playRound (playerSelection,computerSelection) {
         return "Scissors cuts paper. You win!"  
 
     }    
-  }  
+  }
     
 }
 
 //This function loops the game until a player reaches 5 and declares a winner//
-function game() {
-  
-  let round = 0;
-  let playerScore = 0;
-  let computerScore = 0;
+let round = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-  while (playerScore < 5 && computerScore < 5) {
-    //envokes the ComputerChoice function// 
-    computerWeapon = getComputerChoice();
-    // Allows the player to choose their "weapon" via a prompt//
-    playerWeapon = prompt("Choose your weapon!",);
-    let firstChar = playerWeapon[0];
-    let otherChar = playerWeapon.slice(1,);
-    firstChar = firstChar.toUpperCase();
-    otherChar = otherChar.toLowerCase();
-    playerWeapon = firstChar + otherChar;
+const playerScoreCounter = document.querySelector(".player_score");
+const computerScoreCounter = document.querySelector(".comp_score");
 
-    //records the outcome of one round and counts the number of rounds played//
-    let roundOutcome = playRound(playerWeapon,computerWeapon);
-
-    if (roundOutcome.includes("tie")) {
-      alert(roundOutcome + "\n" + "Your score:" + playerScore + " Computer score:" + computerScore);
-      continue;
-    }
-    else {
-    round = round + 1;
-    }
-
-    //Checks for a loss or a win and applies a score for the player or computer//
-    if (roundOutcome.includes("win")) {
-      playerScore = playerScore + 1;
-    }
-    else if (roundOutcome.includes("lose")) {
-      computerScore = computerScore + 1;
-    }
-
-    alert(roundOutcome + "\n" + "Your score:" + playerScore + " Computer score:" + computerScore);
+//This function adds 1 point to each players score depending on the outcome
+function game () {
+  if (outcome.includes("win")) {
+    playerScore += 1;
   }
   
-  console.log(round);
-  console.log(playerScore);
-  console.log(computerScore);
-
-  //compares scores and declares a winner//
-  if (playerScore > computerScore) {
-    alert("Congratulations, you win!");
-  }
-
-  else if (playerScore < computerScore) {
-    alert("Unlucky, you lose!")
+  else if (outcome.includes("lose")) {
+    computerScore += 1;
   }
 }
+
+//This function checks for a winner
+function endGame() {
+  if (computerScore == 5 || playerScore == 5) {
+    if (playerScore > computerScore) {
+      result.textContent = "Congratulations, you win!";
+      playerScore = 0;
+      computerScore = 0;
+    }
+    else if (playerScore < computerScore) {
+      result.textContent = "Unlucky, you lose!";
+      playerScore = 0;
+      computerScore = 0;
+    }
+  }
+}  
+
+//Adds an event listener to the buttons and invokes the above functions//
+const btns = document.querySelectorAll('button');
+const result = document.querySelector(".outcome");
+const playerChoice = document.querySelector(".player");
+const compChoice = document.querySelector(".comp");
+let computerWeapon = "";
+let outcome = "";
+
+btns.forEach((button) => {
+  button.addEventListener('click', () => {
+    computerWeapon = getComputerChoice();
+    outcome = playRound(button.classList.value,computerWeapon);
+    result.textContent = outcome;
+    playerChoice.textContent = "Your choice: " + button.classList.value;
+    compChoice.textContent = "Computer choice: " + computerWeapon;
+    game();
+    playerScoreCounter.textContent = "Player score: " + playerScore;
+    computerScoreCounter.textContent = "Computer score: " + computerScore;
+    endGame ();
+  });
+})
